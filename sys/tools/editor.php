0116535242eac0a1662fb12ed987f3e4a7de6bbd
Malagasy <andyralanto@gmail.com>
2015-03-25 23:08:17 +0100

    .

--- sys/tools/editor.php
+++ sys/tools/editor.php
@@ -11,7 +11,7 @@ function editor_get_files_inside( $folder ){
 
 	foreach( $files as $file ){
 		if( $file[0] == '.' ) continue;
-		if( is_dir( $file ) ){
+		if( is_dir( $folder  . '/' . $file ) ){
 			$files_2[] = $file;
 		}else{
 			if( in_array( file_extension( $file ) , $exts ) )
