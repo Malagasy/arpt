d9a4068f21fb30124af53f4b74655d6ea5cdab70
Malagasy <andyralanto@gmail.com>
2015-03-26 00:57:53 +0100

    .

--- sys/tools/editor.php
+++ sys/tools/editor.php
@@ -52,6 +52,6 @@ function editor_register_file( $path , $content ){
 	}
 
 	if( !file_exists( $path ) ) die("filenotexists");
-	if( file_put_contents($path, htmlentities( $content ) ) === false ) die( "fails");
+	if( file_put_contents($path, ( $content ) ) === false ) die( "fails");
 	die("filechanged");
 }
