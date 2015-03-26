c1be0b165c451af23e632fa45f463204a6929fa4
Malagasy <andyralanto@gmail.com>
2015-03-26 22:17:50 +0100

    .

---
+++ development/themes/arpt/modeles/model-tracker.php
@@ -0,0 +1,40 @@
+<?php
+get_header();
+?>
+
+<div class="container">
+	<div class="row page-header">
+		<div class="col-md-8">
+			<?php
+			$slug = do_slug( implode( '/' , get_pageargs() ) );
+			set_queried( array( 'slug' => $slug ) );
+
+			while( qnext() ) : ?>
+				<h1><?php echo qtitle() ?></h1>
+				<p class="content">
+					<?php echo qcontent(); ?>
+				</p><?php
+			endwhile; 
+			?>
+			</div>
+
+			<p class="content-bottom top-buffer-40">
+				<?php 
+				echo '<i>' . siteslogan() . '</i>';
+				?>
+			</p>
+		</div>
+		<div class="col-md-4 top-buffer-40">
+			<?php
+			load('menus.php');
+			?>
+		</div>
+	</div>
+
+	<div class="row" id="comments">
+		<?php load_part( 'commentaires' ); ?>
+	</div>
+</div>
+<?php
+get_footer();
+?>
