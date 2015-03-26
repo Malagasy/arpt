a34afbabc54d08ee22a74391de3971704ea17aa7
Malagasy <andyralanto@gmail.com>
2015-03-26 21:33:48 +0100

    seo

--- development/themes/arpt/modeles/model-sources.php
+++ development/themes/arpt/modeles/model-sources.php
@@ -13,7 +13,41 @@ get_header();
 				</p><?php
 			endwhile; 
 			?>
-			
+
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-4"><?php
+
+						$page_system_files = scandir( '/' );
+						?>
+						<table class="table table-striped table-hover ">
+							<thead>
+								<tr>
+									<th>Fichiers Page Système</th>
+								</tr>
+							</thead>
+							<tbody><?php
+								foreach( $page_system_files as $file ) : ?>
+								<tr>
+									<td><?php echo a( qlink() . $file , $file ); ?></td>
+								</tr><?php
+								endforeach; ?>
+
+							</tbody>
+						</table>
+					</div>
+
+					<div class="col-md-4">
+					
+					</div>
+					
+					<div class="col-md-4">
+	
+					</div>
+
+				</div>
+			</div>
+
 			<p class="content-bottom top-buffer-40">
 				<?php 
 				echo '<i>' . siteslogan() . '</i>';
