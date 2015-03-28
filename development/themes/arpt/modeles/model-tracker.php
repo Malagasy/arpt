86b6d2b5cb0d949968cbe9eaa98477e69efb76d8
Malagasy <andyralanto@gmail.com>
2015-03-28 02:32:52 +0100

    .

--- development/themes/arpt/modeles/model-tracker.php
+++ development/themes/arpt/modeles/model-tracker.php
@@ -35,7 +35,7 @@ get_header();
 					Dernières modifications : <?php echo arpt_date( $last_edit_file ); ?>
 				</p>
 
-				<div class="view_code_block" style="font-size:11px"><?php echo highlight_string ( trim( ( file_get_contents( $path ) ) ) );	 ?></div>
+				<div class="view_code_block"><?php echo highlight_string ( trim( ( file_get_contents( $path ) ) ) );	 ?></div>
 			</div>
 
 
