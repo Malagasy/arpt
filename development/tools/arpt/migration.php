181134990beb62a4a719208e25af5bc594ad86f2
Malagasy <andyralanto@gmail.com>
2015-03-28 16:48:18 +0100

    .

--- development/tools/arpt/migration.php
+++ development/tools/arpt/migration.php
@@ -235,7 +235,7 @@ function get_prototype_functions(){
 			$content->qnext();
 			if( diffstr( $content->qtype() , 'fonction' ) ) continue;
 			if( strtotime( $content->qproperty('last_edit') ) >=  filemtime( './' . $function['File'] ) ) continue;
-			
+			echo "lastedit:" . strtotime( $content->qproperty('last_edit') . "-----" . filemtime( './' . $function['File'] ) . '<br>';
 			update_content( $content->qid() , $the_args );
 
 				$value = '<pre><code class="php">' . $function['Prototype'] . '</code></pre>';
@@ -295,7 +295,7 @@ function get_prototype_functions(){
 
 				echo 'CustomChamps édités.' . '<br>';
 			}
-
+die();
 		redirect( get_clean_url() );
 		
 	
