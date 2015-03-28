c511c1dbf8a09592dd1ef5961443321477c579d4
Malagasy <andyralanto@gmail.com>
2015-03-28 02:35:52 +0100

    .

--- development/tools/arpt/migration.php
+++ development/tools/arpt/migration.php
@@ -1,16 +1,20 @@
 <?php
 
 function get_prototype_functions(){
-	/*
+
 	$base_path = './sys/';
 	$files[] = 'access.php';
 	$files[] = 'properties.php';
-	logr( $base_path . $files[0] );
+
 	$code = file_get_contents( $base_path . $files[0] );
-	$gets = token_get_all( $code );
 
-	logr( get_defined_functions() );
+	$args = array();
 
-	exit();*/
+	$preg = '/function[\s\n]+(\S+)[\s\n]*\(/';
 
+	preg_match_all( $preg, $code, $args )
+
+	logr($args);
+
+	exit();
 }
