01c0ab983fc4a8ec2417a01afebb3209750b92f2
Malagasy <andyralanto@gmail.com>
2015-03-29 20:15:19 +0200

    .

--- sys/functions/urls.php
+++ sys/functions/urls.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+	* Retourne l'URL de la page actuelle.
+	*
+	* Retourne l'URL de la page actuelle plus les éventuelles paramètres passés en argument.
+	*
+	* @param String $arg Argument ajouté à la fin de l'URL.
+	* @return String
+*/
+
 function get_current_url($arg=null){
 	global $arpt;
 	$url = trimslash( $_SERVER['REQUEST_URI'] );
@@ -15,6 +24,15 @@ function get_current_url($arg=null){
 	return get_site_url() . '/' . $filtered_url . $arg;
 }
 
+/**
+	* Retourne l'URL propre de la page actuelle.
+	*
+	* Retourne l'URL propre de la page actuelle plus les éventuelles paramètres passés en argument.
+	*
+	* @param String $arg Argument ajouté à la fin de l'URL.
+	* @return String
+*/
+
 function get_clean_url( $arg = null ){
 	if( $arg[0] != '/' )
 		$arg = '/' . $arg;
@@ -24,6 +42,15 @@ function get_clean_url( $arg = null ){
 function get_base_url(){
 	return  get_the_scheme() . get_the_host();
 }
+
+/**
+	* Retourne l'URL de la page d'administration.
+	*
+	* Retourne l'URL de la page actuelle plus les éventuelles paramètres passés en argument.
+	*
+	* @param String $name Argument ajouté à la fin de l'URL.
+	* @return String
+*/
 
 function get_admin_url( $name = null ){
 	$name = trimslash( $name );
@@ -47,6 +74,15 @@ function get_site_url( $string = null ){
 	return get_base_url() . $string;
 }
 
+/**
+	* Retourne l'URL de la page recherchée.
+	*
+	* Retourne l'URL de la page recherchée $name.
+	*
+	* @param String $name Argument ajouté à la fin de l'URL.
+	* @return String
+*/
+
 function get_url( $name ){
 	return get_site_url() . clean_path( '/' . $name . '/' );;
 }
@@ -67,9 +103,20 @@ function get_logout_url(){
 	return get_url( 'quit' );
 }
 
+/**
+	* Retourne le chemin du thème actif.
+	*
+	* @return String
+*/
+
 function get_theme_dir(){
 	return './development/themes/'.get_setting('current_theme');
 }
+/**
+	* Retourne le chemin du dossier des uploads.
+	*
+	* @return String
+*/
 function get_upload_dir(){
 	return './development/themes/uploads';
 }
