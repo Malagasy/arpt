6c5ccdab4c5d4293e3464901503921b5cd37e79b
Malagasy <andyralanto@gmail.com>
2015-04-16 20:26:07 +0200

    .

--- development/themes/arpt/modeles/model-blog.php
+++ development/themes/arpt/modeles/model-blog.php
@@ -9,7 +9,7 @@ set_queried( array( 'type' => 'article' ) );
 		<div class="col-md-8 content">
 			<?php
 			while( qnext() ) : ?>
-				<h2><?php echo qtitle() ?><span class="date"><?php echo qdate(); ?></span></h2>
+				<h2><?php echo qtitlelink() ?><span class="date"><?php echo qdate(); ?></span></h2>
 				<p class="contentdata">
 					<?php echo 'Publié le ' . qdate() . ' par ' . qauthor() . '. Dans la catégorie ' . qcategory(); ?>
 				</p>
