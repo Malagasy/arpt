92abb8da24d83495ae2cde7c92a6472554c2ee07
Malagasy <andyralanto@gmail.com>
2015-05-14 21:05:29 +0200

    .

--- development/themes/arpt/modeles/model-blog.php
+++ development/themes/arpt/modeles/model-blog.php
@@ -15,7 +15,7 @@ $articles = get_contents( array( 'type' => 'article' ) );
 				</p>
 				<?php
 				if( $miniature = $articles->qminiature() ) : 
-					echo img( $miniature , array( 'class' => 'img-responsive' , 'height' => 200 ) );
+					echo img( $miniature , array( 'class' => 'img-responsive' , 'style' => 'height:200px' ) );
 				endif;
 				?>
 				<p>
